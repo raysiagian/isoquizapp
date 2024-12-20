8f7cacaab8a288681eb474fcf0e672c6981aaa36
raysiagian <anggasiagian17@gmail.com>
2024-12-20 12:21:31 +0700

    fixing icon navigation on subcategorywrapper appbar

--- lib/views/mainScreen/quizScreen/pages/quizSubCategoryWrapperPage.dart
+++ lib/views/mainScreen/quizScreen/pages/quizSubCategoryWrapperPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_iso_app/models/isoQuizCategoryModel.dart';
+import 'package:quiz_iso_app/routes/appRouteConstants.dart';
 import 'package:quiz_iso_app/styles/localColors.dart';
 import 'package:quiz_iso_app/styles/localTextStyle.dart';
 import 'package:quiz_iso_app/views/mainScreen/quizScreen/pages/quizSubCategoryGradePage.dart';
@@ -96,6 +97,17 @@ class _QuizSubCategoryWrapperPageState
                   style: LocalTextStyle.textTheme.titleLarge!.copyWith(
                     color: Colors.white,
                   ),
+                ),
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                  ),
+                  onPressed: (){
+                    Navigator.pushNamed(
+                    context,
+                    AppRouteConstants.wrapper,
+                );
+                  },
                 ),
                 iconTheme: const IconThemeData(
                   color: Colors.white, // Warna panah kembali
